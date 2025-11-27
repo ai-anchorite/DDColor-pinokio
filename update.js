ac6d7861d85152a7e58584b01a6ac881b1487e99
Anchorite <ai_anchorite@proton.me>
2025-11-27 17:59:38 +1300

    Update update.js

--- update.js
+++ update.js
@@ -4,11 +4,27 @@ module.exports = {
     params: {
       message: "git pull"
     }
-  }, {
+  },{
     method: "shell.run",
     params: {
       path: "app",
-      message: "git pull"
+      message:"git pull"
     }
-  }]
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "env",              
+      path: "app",               
+      message: [
+        "uv pip install -r requirements.txt"
+        ]
+      }
+    },
+    {
+      method: "notify",
+      params: {
+        html: "Update complete. Click the 'start' tab to get started!"
+      }
+    }
+  ]
 }
