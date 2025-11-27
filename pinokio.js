3435350c1bca5288651fb54c8020fd5db791a13f
Anchorite <ai_anchorite@proton.me>
2025-11-27 18:12:34 +1300

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "3.7",
   title: "DDColor",
-  description: "",
+  description: "Image colorization",
   icon: "icon.png",
   menu: async (kernel, info) => {
     let installed = info.exists("app/env")
